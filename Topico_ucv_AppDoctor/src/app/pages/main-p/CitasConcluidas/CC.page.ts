e12935f7d58cfa5fe6577cf4c1376007ee2f7766
Kenith Guanilo <kenithguanilo@gmail.com>
2025-05-24 00:24:46 -0500

    refactor: Clean up code formatting and improve readability in CC.page.ts

--- Topico_ucv_AppDoctor/src/app/pages/main-p/CitasConcluidas/CC.page.ts
+++ Topico_ucv_AppDoctor/src/app/pages/main-p/CitasConcluidas/CC.page.ts
@@ -3,7 +3,12 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { orderBy, where } from 'firebase/firestore';
 import { Cita } from 'src/app/models/cita.model';
-import { collectionGroup, getFirestore, query, onSnapshot } from 'firebase/firestore';
+import {
+  collectionGroup,
+  getFirestore,
+  query,
+  onSnapshot,
+} from 'firebase/firestore';
 import { user } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase_Datos_App.service';
 import { AddUpdateCitaComponent } from 'src/app/shared/components/add-update-cita/add-update-cita.component';
@@ -15,7 +20,6 @@ import { Camara_utilsService } from 'src/app/services/Camara_utils.service';
   styleUrls: ['./CC.page.scss'],
 })
 export class CCPage implements OnInit {
-
   firebaseSvc = inject(FirebaseService);
   utilsSvc = inject(Camara_utilsService);
 
@@ -33,7 +37,6 @@ export class CCPage implements OnInit {
     return fechaCita < now ? 'Cita Concluida' : 'Cita Pendiente';
   }
 
-
   citaEstadoColor(cita): string {
     const fechaCita = new Date(`${cita.fecha}T${cita.hora}:00`);
     const now = new Date();
@@ -42,14 +45,13 @@ export class CCPage implements OnInit {
 
   getCitasConcluidas() {
     const now = new Date();
-    return this.citas.filter(cita => {
+    return this.citas.filter((cita) => {
       const fechaCita = new Date(`${cita.date}T${cita.time}:00`);
       return fechaCita < now; // Filtra solo las citas concluidas
     });
   }
-  
-  ngOnInit() {
-  }
+
+  ngOnInit() {}
 
   user(): user {
     return this.utilsSvc.getFromLocalStorage('user');
@@ -71,7 +73,7 @@ export class CCPage implements OnInit {
     let hoy = new Date();
     hoy.setHours(0, 0, 0, 0); // Establece la hora a medianoche para la comparación
 
-    return this.citas.filter(cita => {
+    return this.citas.filter((cita) => {
       let fechaCita = new Date(cita.date);
       fechaCita.setHours(0, 0, 0, 0); // Establece la hora a medianoche para la comparación
 
@@ -79,35 +81,36 @@ export class CCPage implements OnInit {
     });
   }
 
-  // Obtener las citas
   getProducts() {
     this.loading = true;
-    let user = this.utilsSvc.getFromLocalStorage('user'); // Obtén el usuario actual
+    const user = this.utilsSvc.getFromLocalStorage('user');
 
-    // Utilizamos una consulta de grupo de colecciones para obtener todas las citas
-    let citasQuery = query(
+    if (!user?.name) {
+      console.error('Usuario no válido.');
+      this.loading = false;
+      return;
+    }
+
+    const citasQuery = query(
       collectionGroup(getFirestore(), 'cita'),
-      where('doctor', '==', user.name), // Filtra solo las citas del doctor con el mismo nombre del usuario
-      orderBy('fecha', 'desc')
+      where('doctor', '==', user.name),
+      orderBy('date', 'desc')
     );
 
-    let sub = onSnapshot(citasQuery, (querySnapshot) => {
-      let citas = [];
-      querySnapshot.forEach((doc) => {
-        citas.push(doc.data());
-      });
-      console.log(citas);
-      this.citas = citas;
-      this.loading = false;
-      // No es necesario desuscribirse de onSnapshot
-    }, (error) => {
-      // Maneja el error si es necesario
-    });
+    onSnapshot(
+      citasQuery,
+      (querySnapshot) => {
+        const citas: Cita[] = [];
+        querySnapshot.forEach((doc) => {
+          citas.push(doc.data() as Cita);
+        });
+        this.citas = citas;
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error al obtener citas:', error);
+        this.loading = false;
+      }
+    );
   }
-
- 
-
-  
-
 }
-
