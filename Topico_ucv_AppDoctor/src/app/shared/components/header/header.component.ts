18c97d763997ff859f25cad9dd6e562033e4758b
mmc-byte <marielamata.mmc@gmail.com>
2025-05-24 05:43:09 -0500

    app doctor: doctor en el header, no mas foto, terminos funciona, mejora menor del formato de citas

--- Topico_ucv_AppDoctor/src/app/shared/components/header/header.component.ts
+++ Topico_ucv_AppDoctor/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { Camara_utilsService } from 'src/app/services/Camara_utils.service';
-
+import { FirebaseApp } from '@angular/fire/compat';
+import { FirebaseService } from 'src/app/services/firebase_Datos_App.service';
+import { user } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-header',
@@ -15,11 +17,17 @@ export class HeaderComponent  implements OnInit {
  @Input() backButton!: string;
  //== parametro para que se muestre el boton
 
- utilSvc = inject(Camara_utilsService);
+  utilSvc = inject(Camara_utilsService);
+  firebaseSvc = inject(FirebaseService);
 
-  
+
 
   ngOnInit() {}
+
+  user(): user {
+    return this.utilSvc.getFromLocalStorage('user');
+  }
+
 
   dismissModal(){
     this.utilSvc.dismissModal();
